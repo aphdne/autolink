b0cb6cd8e24b62eb8dd90b78dd06d629d11d29b4
aphd <aphidarium@proton.me>
2025-08-22 21:44:33 +0100

    editor extension to show note links

--- main.ts
+++ main.ts
@@ -1,4 +1,92 @@
+import { Decoration, DecorationSet, ViewUpdate, PluginValue, PluginSpec, EditorView, ViewPlugin, WidgetType } from '@codemirror/view';
+import { RangeSetBuilder } from '@codemirror/state';
+import { syntaxTree } from '@codemirror/language';
 import { Plugin } from 'obsidian';
+
+function registerExtension(plugin: Plugin) {
+	class AutolinkWidget extends WidgetType {
+		toDOM(view: EditorView): HTMLElement {
+			const div = document.createElement('span');
+
+			div.innerText = '👉';
+
+			return div;
+		}
+	}
+
+	class AutolinkExtension implements PluginValue {
+		decorations: DecorationSet;
+		markdownFiles: TFile[];
+
+		constructor(view: EditorView) {
+			this.markdownFiles = plugin.app.vault.getMarkdownFiles();
+			this.decorations = this.buildDecorations(view);
+		}
+
+		update(update: ViewUpdate) {
+			if (update.docChanged || update.viewportChanged) {
+				this.decorations = this.buildDecorations(update.view);
+			}
+		}
+
+		destroy() {}
+
+		buildDecorations(view: EditorView): DecorationSet {
+			const builder = new RangeSetBuilder<Decoration>();
+			const markdownFiles = this.markdownFiles;
+
+			if (!markdownFiles) {
+				return;
+			}
+
+			for (let { from, to } of view.visibleRanges) {
+				syntaxTree(view.state).iterate({ from, to, enter(node: Node) {
+					if (node.type.name.startsWith('Document')) {
+						const text = view.state.doc.sliceString(node.from, node.to);
+
+						let matches = [];
+						// https://regex101.com/r/9eA7Sl/5
+						// 1st capture group captures $term within wikilinks, to filter them out
+						// 2nd capture group captures $term, except for within headers and tags, and including with an -ed, -es, or -s suffix to allow for plurals etc.
+						markdownFiles.forEach((mdf) => {
+							const re  = `(?<=\\[\\[.*)${mdf.basename}(?![^\\]\\]]*\\[\\[)(?=.*\\]\\])|((?<!\\#|^\\|.*|^\\#.*)\\b${mdf.basename}[es]?s?[ed]?\\b)`;
+							const links = [...text.matchAll(new RegExp(re, "gmi"))];
+							links.forEach((link) => {
+								if (link[1]) {
+									matches.push({ name: mdf.basename, link: link});
+								}
+							})
+						});
+
+						matches.sort((a, b) => {
+							if (a.link.index > b.link.index)
+								return 1;
+							else if (a.link.index == b.link.index)
+								return 0;
+							return -1;
+						});
+
+						matches.forEach((match) => {
+							builder.add(match.link.index, match.link.index + match.name.length, Decoration.mark({
+								class: "autolink-link",
+								tagName: "a",
+							}));
+						});
+					}
+				},
+			});
+		}
+
+		return builder.finish();
+	  }
+	}
+
+	const pluginSpec: PluginSpec<AutolinkExtension> = {
+	  decorations: (value: AutolinkExtension) => value.decorations,
+	};
+
+	plugin.registerEditorExtension(ViewPlugin.fromClass(AutolinkExtension, pluginSpec));
+}
 
 function toLink(link: string, display: string = link) {
 	return `<a data-href="${link}" href="${link}" class="internal-link autolink-link" target="_blank" rel="noopener nofollow">${display}</a>`;
@@ -6,6 +94,8 @@ function toLink(link: string, display: string = link) {
 
 export default class Autolink extends Plugin {
 	async onload() {
+		registerExtension(this);
+
 		this.registerMarkdownPostProcessor((el, ctx) => {
 			if (!el.hasClass("el-p")) // exclude links, headers, etc.
 				return
