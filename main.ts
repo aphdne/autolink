b125065415d44c3f2396cbc202ab459a09dad745
aphd <aphidarium@proton.me>
2025-08-22 16:16:01 +0100

    basic link insertion post processing

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
 import { Plugin } from 'obsidian';
 
+function toLink(link: string, display: string = link) {
+	return `<a data-href="${link}" href="${link}" class="internal-link autolink-link" target="_blank" rel="noopener nofollow">${display}</a>`;
+}
+
 export default class Autolink extends Plugin {
 	async onload() {
 		this.registerMarkdownPostProcessor((el, ctx) => {
 			if (!el.hasClass("el-p"))
 				return
 
-			let text = el.innerHTML;
-
 			this.app.vault.getMarkdownFiles().reverse().forEach((mdf) => {
-				if (!text.contains(mdf.basename))
+				// https://regex101.com/r/xBzjHB/1
+				const matches = [...el.innerHTML.matchAll(new RegExp(`(?<!\\<a\\>)${mdf.basename}(?!.*\\<\\/a\\>)`, "gmi"))];
+				if (matches.length == 0)
 					return;
-				console.log(el, mdf.basename);
+
+				for (const m of matches) {
+					el.innerHTML = el.innerHTML.replaceAll(m[0], toLink(m[0]));
+				}
 			});
 		});
 	}
