5b5e264a2e6e60ea7891a8eb256772e3a74cf7a3
aphd <aphidarium@proton.me>
2025-08-22 18:45:06 +0100

    destructively insert links command

--- main.ts
+++ main.ts
@@ -27,6 +27,53 @@ export default class Autolink extends Plugin {
 				}
 			});
 		});
+
+		this.addCommand({
+			id: "destructively-insert-all-note-links",
+			name: "Destructively insert all note links",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				let inCodeblock = false;
+				let inFrontmatter = false;
+				for (let i = 0; i < editor.lineCount(); i++) {
+					let line = editor.getLine(i);
+
+					if (i == 0 && line == "---")
+					  inFrontmatter = true;
+
+					if (inFrontmatter && line == "---")
+					  inFrontmatter = false;
+
+					if (line.startsWith("```"))
+					  inCodeblock = !inCodeblock;
+
+					if (inCodeblock || inFrontmatter)
+					  continue;
+
+					this.app.vault.getMarkdownFiles().reverse().forEach((mdf) => {
+						const term = mdf.basename.replace("+", "\\+").replace(".", "\\.");
+
+						// https://regex101.com/r/9eA7Sl/5
+						// 1st capture group captures $term within wikilinks, to filter them out
+						// 2nd capture group captures $term, except for within headers and tags, and including with an -ed, -es, or -s suffix to allow for plurals etc.
+						const re  = `(?<=\\[\\[.*)${term}(?![^\\]\\]]*\\[\\[)(?=.*\\]\\])|((?<!\\#|^\\|.*|^\\#.*)\\b${term}[es]?s?[ed]?\\b)`;
+						const matches = [...line.matchAll(new RegExp(re, "gmi"))].reverse();
+
+						if (matches.length == 0 || this.app.workspace.activeEditor.file == mdf)
+							return;
+
+						for (const m of matches) {
+							// index with [1] to use 2nd capture group
+							if (!m[1])
+							  continue;
+
+							line = line.replaceAll(m[1], "[[" + mdf.name + "|" + m[1] + "]]");
+						}
+					});
+
+					editor.setLine(i, line);
+				}
+			}
+		});
 	}
 
 	onunload() {
